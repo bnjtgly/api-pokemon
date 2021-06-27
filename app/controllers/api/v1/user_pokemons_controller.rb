4f4212219bf1e12db5165323fbc9bd92b067c10f
bnjtgly <benjibtigley@gmail.com>
2021-06-27 15:56:36 +0800

    Minor revisions to endpoint

--- app/controllers/api/v1/user_pokemons_controller.rb
+++ app/controllers/api/v1/user_pokemons_controller.rb
@@ -24,27 +24,34 @@ class Api::V1::UserPokemonsController < ApplicationController
     end
 
     def update
-        my_pokemon = Info.where(pokemon_id: @my_pokemon.id)
-        pokemon_info = Info.select("pokemons.name, infos.*").joins(:pokemon).where(pokemon_id: @my_pokemon.id)
+        # pokemon_id = UserPokemon.find(params[:id])
+        # my_pokemon = Info.where(pokemon_id: pokemon_id.pokemon_id)
+        my_pokemon = Info.where(pokemon_id: params[:id])
+        # pokemon_info = Info.select("pokemons.name, infos.*").joins(:pokemon).where(pokemon_id: pokemon_id.pokemon_id)
+        pokemon_info = Info.select("pokemons.name, infos.*").joins(:pokemon).where(pokemon_id: params[:id])
         if my_pokemon.update(my_pokemon_params[:info])
-            render json: pokemon_info
+            render json: {message: 'Success'}, status: :ok
         else
-            render json: { errors: my_pokemon.errors.full_messages }, status: :unprocessable_entity
+            render json: { message: 'Error', errors: my_pokemon.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
     def destroy
-        my_pokemon = UserPokemon(@my_pokemon.id)
-        render json: my_pokemon
+        my_pokemon = UserPokemon.where(pokemon_id: params[:id])
+        UserPokemon.destroy(my_pokemon.first.id)
+        render json: {message: 'Pokemon Deleted'}, status: :ok
     end
 
     private
     def find_pokemon
+        # poke_id = UserPokemon.where(pokemon_id: params[:id])
+        # @my_pokemon = poke_id
         @my_pokemon = Pokemon.find(params[:id])
+        # @my_pokemon = Pokemon.find(poke_id.first.id)
         rescue ActiveRecord::RecordNotFound
         render json: { errors: 'Pokemon not found' }, status: :not_found
     end
     def my_pokemon_params
-        params.require(:user_pokemon).permit(info: [:pokedex_id, :nick_name, :moveset, pokemon_attributes: [:name]])
+        params.require(:user_pokemon).permit(info: [:pokedex_id, :nick_name, :favorite_color, :favorite_food, :moveset, pokemon_attributes: [:name]])
     end
 end
